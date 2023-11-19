8672f47484c8368f0a0040f424ebfe0229eb9370
Mostafa <mosti.asghari@gmail.com>
2023-11-18 17:19:57 -0800

    Add cors

--- eravista-api/index.js
+++ eravista-api/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { parseTimeline } from "./history-scraper.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors({ origin: "http://localhost:3000" }));
+
 app.get("/", function (req, res) {
   parseTimeline().then((timeline) => {
     res.send(timeline);
