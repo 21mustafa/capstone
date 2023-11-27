3f84914e1410dc29b95b0e4625060daa9face91f
Mostafa <mosti.asghari@gmail.com>
2023-11-26 16:30:24 -0800

    Add mobile view

--- eravista-frontend/src/components/Filter/Filter.jsx
+++ eravista-frontend/src/components/Filter/Filter.jsx
@@ -7,7 +7,7 @@ const FilterSection = (props) => {
       <div className="filter__section-label">{props.label}</div>
 
       <div className="filter__section-list">
-        {props.elements.map(({ id, label, value }) => {
+        {props.elements?.map(({ id, label, value }) => {
           return (
             <div className="filter__section-item">
               <input
@@ -63,85 +63,178 @@ function Filter(props) {
 
   return (
     <div className={`filter ${props.open ? "filter--open" : ""}`}>
-      <FilterSection
-        elements={
-          props.timeline
-            ? props.timeline.map((element) => ({
-                id: element._id,
-                label: element.century,
-              }))
-            : []
-        }
-        name={"century"}
-        checked={centuryFilter}
-        onChange={({ id, value }) => {
-          setCenturyFilter({ id, value });
-
-          const year = props.timeline?.find(
-            (element) => element.century === value
-          ).events[0];
-          setYearFilter({
-            id: year._id,
-            value: year.year,
-          });
-
-          const event = props.timeline?.find(
-            (element) => element.century === value
-          ).events[0].events[0];
-          setEventFilter({ id: event._id, value: event.date });
-        }}
-        label="Select a cenutury"
-      />
-
-      {centuryFilter && (
+      <div className="filter--desktop">
         <FilterSection
           elements={
             props.timeline
-              ? props.timeline
-                  .find((element) => element._id === centuryFilter.id)
-                  .events.map((element) => ({
-                    label: element.year,
-                    id: element._id,
-                  }))
+              ? props.timeline.map((element) => ({
+                  id: element._id,
+                  label: element.century,
+                }))
               : []
           }
-          name={"year"}
-          checked={yearFilter}
+          name={"century"}
+          checked={centuryFilter}
           onChange={({ id, value }) => {
-            setYearFilter({ id, value });
+            setCenturyFilter({ id, value });
 
-            const event = props.timeline
-              ?.find((element) => element._id === centuryFilter.id)
-              .events?.find((element) => element._id === id).events[0];
-            setEventFilter({
-              id: event._id,
-              value: event.date,
+            const year = props.timeline?.find(
+              (element) => element.century === value
+            ).events[0];
+            setYearFilter({
+              id: year._id,
+              value: year.year,
             });
-          }}
-          label="Select a year"
-        />
-      )}
 
-      {centuryFilter && yearFilter && (
-        <FilterSection
-          elements={
-            props.timeline
-              ? props.timeline
-                  .find((element) => element._id === centuryFilter.id)
-                  .events.find((element) => element._id === yearFilter.id)
-                  .events.map((element) => ({
-                    id: element._id,
-                    label: element.date,
-                    value: element.description,
-                  }))
-              : []
-          }
-          name={"event"}
-          checked={eventFilter}
-          onChange={({ id, value }) => setEventFilter({ id, value })}
-          label={"Select an event"}
+            const event = year.events[0];
+            setEventFilter({ id: event._id, value: event.date });
+          }}
+          label="Select a cenutry"
         />
-      )}
+
+        {centuryFilter && (
+          <FilterSection
+            elements={
+              props.timeline
+                ? props.timeline
+                    .find((element) => element._id === centuryFilter.id)
+                    .events.map((element) => ({
+                      label: element.year,
+                      id: element._id,
+                    }))
+                : []
+            }
+            name={"year"}
+            checked={yearFilter}
+            onChange={({ id, value }) => {
+              setYearFilter({ id, value });
+              const event = props.timeline
+                ?.find((element) => element._id === centuryFilter.id)
+                .events?.find((element) => element._id === id).events[0];
+              setEventFilter({
+                id: event._id,
+                value: event.date,
+              });
+            }}
+            label="Select a year"
+          />
+        )}
+
+        {centuryFilter && yearFilter && (
+          <FilterSection
+            elements={
+              props.timeline
+                ? props.timeline
+                    .find((element) => element._id === centuryFilter.id)
+                    ?.events.find((element) => element._id === yearFilter.id)
+                    ?.events.map((element) => ({
+                      id: element._id,
+                      label: element.date,
+                      value: element.description,
+                    }))
+                : []
+            }
+            name={"event"}
+            checked={eventFilter}
+            onChange={({ id, value }) => setEventFilter({ id, value })}
+            label={"Select an event"}
+          />
+        )}
+      </div>
+      <div className="filter--mobile">
+        <div className="filter__select">
+          <label for="century-select">
+            <span>Select a century</span>
+            <select
+              name="century"
+              id="century-select"
+              onChange={(e) => {
+                const centuryID = e.target.value;
+                const century = props.timeline.find(
+                  (century) => century._id === centuryID
+                );
+                setCenturyFilter({ id: centuryID, value: century.century });
+
+                const year = century?.events[0];
+                setYearFilter({ id: year._id, value: year.year });
+
+                const event = year?.events[0];
+                setEventFilter({ id: event._id, value: event.date });
+              }}
+            >
+              {props.timeline?.map((century) => (
+                <option
+                  value={century._id}
+                  selected={century._id === centuryFilter?.id}
+                >
+                  {century.century}
+                </option>
+              ))}
+            </select>
+          </label>
+        </div>
+
+        <div className="filter__select">
+          <label for="year-select">
+            <span>Select a year</span>
+            <select
+              name="year"
+              id="year-select"
+              onChange={(e) => {
+                const yearID = e.target.value;
+                const year = props.timeline
+                  .find((century) => century._id === centuryFilter.id)
+                  .events.find((element) => element._id === yearID);
+                setYearFilter({ id: yearID, value: year.year });
+
+                const event = year?.events[0];
+                setEventFilter({ id: event._id, value: event.date });
+              }}
+            >
+              {props.timeline
+                ?.find((element) => element._id === centuryFilter?.id)
+                ?.events?.map((year) => (
+                  <option
+                    value={year._id}
+                    selected={year._id === yearFilter?.id}
+                  >
+                    {year.year}
+                  </option>
+                ))}
+            </select>
+          </label>
+        </div>
+
+        <div className="filter__select">
+          <label for="event-select">
+            <span>Select a event</span>
+            <select
+              name="event"
+              id="event-select"
+              onChange={(e) => {
+                const eventID = e.target.value;
+                const event = props.timeline
+                  .find((century) => century._id === centuryFilter.id)
+                  .events.find((element) => element._id === yearFilter.id)
+                  .events.find((element) => element._id === eventID);
+                setEventFilter({ id: event._id, value: event.date });
+              }}
+            >
+              {props.timeline
+                ?.find((element) => element._id === centuryFilter?.id)
+                ?.events?.find((element) => element._id === yearFilter.id)
+                ?.events?.map((event) => (
+                  <option
+                    value={event._id}
+                    selected={event._id === eventFilter?.id}
+                  >
+                    {event.date ? event.date : "Event"}
+                  </option>
+                ))}
+            </select>
+          </label>
+        </div>
+      </div>
     </div>
   );
 }
